865423d316c477001a1aa9c00738f9a6d7b0aaba
Summer8660 <bassume10@gmail.com>
2024-10-20 06:26:56 +0000

    Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 241341
+let accountEmail = "bassume10@gmail.com"
+var accountPassword = 123456
+accountCiy = "Pune"
+
+/*
+prefer not to use var
+becouse of issue in block scope and functional scope
+*/
+
+//accountId = 2
+console.log (accountId)
+console.table([accountId,accountEmail,accountPassword,accountCiy])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js code as newer version 
+
+
+alert(3+3) // we are using node.js not a browser
+
+
+console.log (2+3) //code readability should be high
+
+let name= "basayya"
+let age= 25
+let city=pune
+
+console.log(typeof undefined ) // undefined
+console.log(typeof null ) //object
